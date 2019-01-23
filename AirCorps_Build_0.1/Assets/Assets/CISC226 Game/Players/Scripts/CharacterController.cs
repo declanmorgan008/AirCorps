e4318df58a0275359b14d638725e4806ad0831d2
Matt Denny <mattdenny001@hotmail.com>
2019-01-23 14:20:09 -0500

    Init Repo
    This is just the first update to move everything to github

---
+++ AirCorps_Build_0.1/Assets/Assets/CISC226 Game/Players/Scripts/CharacterController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private float moveSpeed = 2.3f;
+    private float accel = 5f;
+    private float dCoef = 4f;
+    private float maxAngle = 5f;
+    private float dMax = 0.1f;
+    private float rotationSpeed = 3f;
+    private float angle;
+    private float targetAngle;
+    private float weight;
+    private float deltaAngle = 0;
+    private float boostingTrigger = 0;
+    private bool boostingBumber = false;
+    float gravity = 0.3f;
+
+    private Vector2 inputDirection = Vector2.zero;
+    private Vector2 inputDirectionLast = Vector2.zero;
+
+    private Vector2 velocity;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        velocity.x = moveSpeed;
+        velocity.y = 0;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        inputDirectionLast = inputDirection;
+        inputDirection = Vector2.zero;
+        inputDirection.x = Input.GetAxis("Horizontal");
+        inputDirection.y = Input.GetAxis("Vertical");
+        if (inputDirection == Vector2.zero)
+        {
+            inputDirection = inputDirectionLast;
+        }
+
+        boostingTrigger = Input.GetAxis("BoosterTrigger");
+        boostingBumber = Input.GetButton("BoosterBumper");
+
+        angle = transform.eulerAngles.z;
+        targetAngle = Vector2.Angle(Vector2.right, inputDirection);
+
+        if (inputDirection.y < 0)
+        {
+            targetAngle = 360 - targetAngle;
+        }
+
+        if (targetAngle > 90 && targetAngle < 270 && (angle <= 90 || angle >= 270))
+        {
+            spriteRenderer.flipY = !spriteRenderer.flipY;
+        } else if ((targetAngle <= 90 || targetAngle >= 270) && angle > 90 && angle < 270)
+        {
+            spriteRenderer.flipY = !spriteRenderer.flipY;
+        }
+
+        if (boostingTrigger == 0 && !boostingBumber)
+        {
+            deltaAngle = Mathf.Min(Mathf.Abs(targetAngle - angle), 360 - Mathf.Abs(targetAngle - angle));
+
+            
+            deltaAngle = Mathf.Min(deltaAngle, maxAngle);
+
+            
+            if(angle > targetAngle && !(angle >= 270 && targetAngle <= 90) 
+                && !(angle > 180 && targetAngle < 90 && angle - targetAngle > 180)
+                || (targetAngle >= 270 && angle < 90))
+            {
+                deltaAngle = -deltaAngle;
+            }
+
+            //deltaAngle = Mathf.Sign(Vector2.SignedAngle(rb2d.velocity.normalized, inputDirection.normalized))*deltaAngle;
+
+            Debug.Log("Angle: " + angle + ", Target: " + targetAngle + ", Delta: " + deltaAngle);
+
+            Rotate(transform, deltaAngle);
+            angle = transform.eulerAngles.z;
+
+            velocity.x = Mathf.Cos(Mathf.Deg2Rad * angle) * accel;
+            velocity.y = Mathf.Sin(Mathf.Deg2Rad * angle) * accel;
+
+            ThrustForward(velocity);
+            ApplyDrag();
+            ClampVelocity();
+        }
+        else
+        {
+            Rotate(transform, targetAngle - angle);
+            angle = transform.eulerAngles.z;
+        }
+    }
+
+    void Movement(Vector2 move)
+    {
+        rb2d.position = rb2d.position + move;
+    }
+
+    void ClampVelocity()
+    {
+        float x = rb2d.velocity.x;
+        float y = rb2d.velocity.y;
+        float mag = rb2d.velocity.magnitude;
+
+        if(mag > moveSpeed)
+        {
+            x = (x / mag) * moveSpeed;
+            y = (y / mag) * moveSpeed;
+        }
+
+        rb2d.velocity = new Vector2(x, y);
+    }
+
+    void ThrustForward(Vector2 force)
+    {
+        rb2d.AddForce(force);
+    }
+
+    void ApplyDrag()
+    {
+        float x = rb2d.velocity.x;
+        float y = rb2d.velocity.y;
+        float targetX = velocity.x;
+        float targetY = velocity.y;
+        float mag = rb2d.velocity.magnitude;
+        float speed = mag;
+        Vector2 delta = Vector2.zero;
+
+        mag = inputDirection.magnitude;
+        targetX = (targetX / mag) * speed;
+        targetY = (targetY / mag) * speed;
+
+        if(Mathf.Abs(targetX) < Mathf.Abs(x))
+        {
+            delta.x = (targetX - x) * dCoef;
+        }
+
+        if (Mathf.Abs(targetY) < Mathf.Abs(y))
+        {
+            delta.y = (targetY - y) * dCoef;
+        }
+
+        ThrustForward(delta);
+    }
+
+    void Rotate(Transform t, float amount)
+    {
+        t.Rotate(0, 0, amount);
+    }
+}
---
+++ AirCorps_Build_0.1/Assets/Assets/CISC226 Game/Players/Scripts/CharacterControllerMouse.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerMouse : MonoBehaviour
+{
+
+    private float moveSpeed = 1f;
+    private float angle = 0;
+    private float angleRate = 0;
+    private float restRate = -24f;
+    private float turnRate = -20f;
+    private float maxRate = 6f;
+    private char dir = 'r';
+    private bool alive = true;
+    private bool hasTurn = false;
+
+    private Vector2 velocity;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        velocity.x = moveSpeed;
+        velocity.y = 0;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 direction = new
+
+        //angle = Vector2.Angle(rb2d.position, pz);
+        //float deltaAngle = angle;
+
+        //Debug.Log("Angle: " + (deltaAngle));
+
+        /*
+        if (Input.GetButton("Jump"))
+        {
+
+            hasTurn = false;
+
+            if (dir == 'r')
+            {
+                if (Mathf.Sign(angleRate) == 1f) { angleRate = 0f; }
+                angleRate += turnRate * Time.deltaTime;
+                if (angleRate < -maxRate) { angleRate = -maxRate; }
+            }
+            else
+            {
+                if (Mathf.Sign(angleRate) == -1f) { angleRate = 0f; }
+                angleRate -= turnRate * Time.deltaTime;
+                if (angleRate > maxRate) { angleRate = maxRate; }
+            }
+        }
+        else
+        {
+
+            if (dir == 'r' && !hasTurn)
+            {
+                if (angle <= 270 && angle > 90 && !hasTurn)
+                {
+                    if (angle <= 270 && angle >= 263) { deltaAngle = 270; hasTurn = true; angleRate = 0; }
+                    dir = 'l';
+                    spriteRenderer.flipY = !spriteRenderer.flipY;
+                }
+                else
+                {
+                    angleRate -= turnRate * Time.deltaTime;
+                    if (angleRate > 4f) { angleRate = 4f; }
+                }
+
+            }
+            else if (dir == 'l' && !hasTurn)
+            {
+                if ((angle > 270 || angle <= 90) && !hasTurn)
+                {
+                    if (angle > 270 && angle <= 277) { deltaAngle = 270; hasTurn = true; angleRate = 0; }
+                    dir = 'r';
+                    spriteRenderer.flipY = !spriteRenderer.flipY;
+                }
+                else
+                {
+                    angleRate += turnRate * Time.deltaTime;
+                    if (angleRate < -4f) { angleRate = -4f; }
+                }
+            }
+        }
+
+        deltaAngle += angleRate;
+
+        Debug.Log("Angle: " + (deltaAngle));
+
+        transform.Rotate(Vector3.forward * (angle - deltaAngle));
+
+        
+        velocity.x = Mathf.Cos(Mathf.Deg2Rad * deltaAngle) * moveSpeed;
+        velocity.y = Mathf.Sin(Mathf.Deg2Rad * deltaAngle) * moveSpeed;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.right * deltaPosition.x;
+        rb2d.position = rb2d.position + move;
+        move = Vector2.up * deltaPosition.y;
+        rb2d.position = rb2d.position + move;*/
+
+        /* This is just a reference to how the movement works
+        
+        velocity.x = moveSpeed;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.right * deltaPosition.x;
+
+        Movement(move);*/
+
+    }
+
+    void Movement(Vector2 move)
+    {
+        rb2d.position = rb2d.position + move;
+    }
+}
+
